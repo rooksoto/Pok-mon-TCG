2117bd262cb1da722fe0aecef1051b4fb489eda4
Rook Soto <rooksoto@gmail.com>
2021-01-31 00:44:38 -0500

    Adds Temp ToDo File

---
+++ app/src/main/java/com/rooksoto/pokmontcgcardviewer/Todo.kt
@@ -0,0 +1,9 @@
+package com.rooksoto.pokmontcgcardviewer
+
+// List global To Do items here so they show up in AS tool window
+
+// TODO: 1/31/21 DELETE THIS FILE when the below have been addressed to an acceptable degree
+
+// TODO: 1/31/21 Clean up Gradle imports (it's super lazy right now)
+// TODO: 1/31/21 Update whatever libs won't break things
+// TODO: 1/31/21 I'm going to use RxJava for networking, coroutines for database - remember to refactor RxJava out
